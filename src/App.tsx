599101b626604e14ed20491adb016c76008aa73f
Justin Cooke <justin.cooke@adrennial.com>
2024-06-04 12:05:34 +0100

    Add upload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import { Authenticator, Flex, ThemeProvider } from '@aws-amplify/ui-react'
-import { TextAreaField, Divider } from '@aws-amplify/ui-react';
-import { StorageManager } from '@aws-amplify/ui-react-storage';
-import {  } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css'
+import { Authenticator, Flex, ThemeProvider } from "@aws-amplify/ui-react";
+import { TextAreaField, Divider } from "@aws-amplify/ui-react";
+import React, { useState } from "react";
 
-const UploadAPicture = () => {
-  return (
-    <StorageManager
-      acceptedFileTypes={['image/*']}
-      path="public/"
-      maxFileCount={1}
-      isResumable
-    />
-  );
-};
+import {} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 const GenAIForm = () => {
+  const [file, setFile] = useState<string | undefined>();
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.files);
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(URL.createObjectURL(e.target.files[0]));
+    }
+  }
+
   return (
-  <Flex
-    width="360px"
-    height="640px"
-    overflow="hidden"
-    position="relative"
-    backgroundColor="rgba(255,255,255,1)"
-  >
     <Flex
       gap="21px"
       direction="column"
@@ -38,31 +29,40 @@ const GenAIForm = () => {
         label="Please enter prompt for AI"
         placeholder="Placeholder"
       />
-      <UploadAPicture/>
-      <Divider
-        width="300px"
-        height="5px"
+      <h2>Upload your image</h2>
+      <input
+        type="file"
+        id="img"
+        name="img"
+        accept="image/*"
+        onChange={handleChange}
       />
+      <img src={file} alt="Uploaded image" width="300px" height="300px" />
+      <Divider width="300px" height="5px" />
     </Flex>
-  </Flex>
-  )
-}
-
+  );
+};
 
 function App() {
-
-
   return (
     <ThemeProvider>
-      <Authenticator socialProviders={['google']}>
+      <Authenticator socialProviders={["google"]}>
         {({ signOut, user }) => (
           <main>
-            <h1>AI Kitchen Generator</h1>
-            <p>Welcome {user?.userId}</p>
-            <GenAIForm/>
-            <button onClick={signOut}>Sign out</button>
-            </main>
-          )}
+            <Flex
+              width="360px"
+              height="640px"
+              overflow="hidden"
+              position="relative"
+              backgroundColor="rgba(255,255,255,1)"
+            >
+              <h1>AI Kitchen Generator</h1>
+              <p>Welcome {user?.userId}</p>
+              <GenAIForm />
+              <button onClick={signOut}>Sign out</button>
+            </Flex>
+          </main>
+        )}
       </Authenticator>
     </ThemeProvider>
   );
