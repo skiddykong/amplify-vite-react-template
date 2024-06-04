fedf5268772ef438a8d7f2a1b5ce8f2d15de594d
Justin Cooke <justin.cooke@adrennial.com>
2024-06-04 09:54:30 +0100

    Simplify codebase, add genAI form

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,58 @@
-import { Authenticator, Fieldset, Flex, ThemeProvider } from '@aws-amplify/ui-react'
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { TextAreaField } from '@aws-amplify/ui-react';
-import { Menu, MenuItem, View, Divider } from '@aws-amplify/ui-react';
+import { Authenticator, Flex, ThemeProvider } from '@aws-amplify/ui-react'
+import { TextAreaField, Divider, Button } from '@aws-amplify/ui-react';
+import {  } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
-import Gallery from './gallery'
 
-const client = generateClient<Schema>();
-
-export const MenuItemsExample = () => {
+function GenAIForm() {
   return (
-    <View width="4rem">
-      <Menu>
-        <MenuItem onClick={() => alert('Download')}>Download</MenuItem>
-        <MenuItem onClick={() => alert('Create a Copy')}>
-          Create a Copy
-        </MenuItem>
-        <Divider />
-        <MenuItem isDisabled onClick={() => alert('Delete')}>
-          Delete
-        </MenuItem>
-      </Menu>
-    </View>
-  );
-};
+  <Flex
+    width="360px"
+    height="640px"
+    overflow="hidden"
+    position="relative"
+    backgroundColor="rgba(255,255,255,1)"
+  >
+    <Flex
+      gap="21px"
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      position="absolute"
+      top="139px"
+      left="30px"
+    >
+      <TextAreaField
+        width="300px"
+        label="Please enter prompt for AI"
+        placeholder="Placeholder"
+      />
+      <Button
+        width="302px"
+      >
+        Upload A Photo
+      </Button>
+      <Divider
+        width="300px"
+        height="5px"
+      />
+    </Flex>
+  </Flex>
+  )
+}
+
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
 
   return (
     <ThemeProvider>
       <Authenticator socialProviders={['google']}>
         {({ signOut, user }) => (
           <main>
-            <h1>Hello {user?.username} Here are the todos</h1>
-            <button onClick={createTodo}>+ new</button>
-            <Gallery/>
-            <ul>
-              {todos.map((todo) => (
-                <li
-                  onClick={() => deleteTodo(todo.id)}
-                  key={todo.id}>
-                  {todo.content}
-                </li>
-              ))}
-            </ul>
-              <div>
-                <Fieldset legend="Kitchen design">
-                  <Flex direction="column">
-                <TextAreaField
-                  descriptiveText="Please enter a prompt for the kitchen you would like to see"
-                  label="AI Prompt"
-                  name="gen_ai_prompt"
-                  placeholder="I would like a kitchen in the style of a modern farmhouse with a large island and a double oven."
-                  rows={3} />
-                <MenuItemsExample/>
-                </Flex>
-                </Fieldset>
-              </div>
-              <button onClick={signOut}>Sign out</button>
+            <h1>AI Kitchen Generator</h1>
+            <p>Welcome {user?.userId}</p>
+            <GenAIForm/>
+            <button onClick={signOut}>Sign out</button>
             </main>
           )}
       </Authenticator>
@@ -82,5 +61,3 @@ function App() {
 }
 
 export default App;
-
-// userA   carpT1ckler
