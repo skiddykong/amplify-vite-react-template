655d716a7b7339d30dae2601086d39eee24fa0b0
Justin Cooke <justin.cooke@adrennial.com>
2024-07-05 13:54:46 +0100

    Refactor into components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Authenticator, Grid, Card, Button, Flex } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Grid,
+  Card,
+  Button,
+  Flex,
+  ScrollView,
+} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import TextToImagePage from "./pages/TextToImage/TextToImagePage";
 
@@ -10,32 +17,49 @@ function App() {
   return (
     <Authenticator socialProviders={["google"]}>
       {({ signOut }) => (
-        <Grid>
-        <Card>
-          <h1>AI Kitchen Generator</h1>
-          <p>Welcome</p>
-        </Card>
-          <Router>
-            <Flex>
-              <Link to="/src/pages/Home" >Home</Link>
-              <Link to="/src/pages/TextToImage">Text To Image Generator</Link>
-              <Link to="/src/pages/About">About</Link>
-            </Flex>
-            <Routes>
-              <Route
-                path="/src/pages/TextToImage"
-                element={<TextToImagePage />}
-              />
-              <Route path="/src/pages/Home" element={<Home />} />
-              <Route path="/src/pages/About" element={<About />} />
-            </Routes>
-          </Router>
-          <Card>
-            <Button style={{ flexGrow: 3 }} onClick={signOut}>
-              Sign out
-            </Button>
-          </Card>
-        </Grid>
+        <ScrollView width="100%" height="10000px" maxWidth="580px">
+          <Grid
+            columnGap="0.5rem"
+            rowGap="0.5rem"
+            templateColumns="1fr"
+            templateRows="3fr"
+            position="fixed"
+            top="0px"
+            left="0px"
+          >
+            <Card columnStart="1" columnEnd="-1">
+              <h1>AI Kitchen Generator</h1>
+              <p>Welcome</p>
+            </Card>
+            <Router>
+              <Flex
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                alignContent="stretch"
+                wrap="nowrap"
+                gap="1rem"
+              >
+                <Link to="/src/pages/Home">Home</Link>
+                <Link to="/src/pages/TextToImage">Text To Image Generator</Link>
+                <Link to="/src/pages/About">About</Link>
+              </Flex>
+              <Routes>
+                <Route
+                  path="/src/pages/TextToImage"
+                  element={<TextToImagePage />}
+                />
+                <Route path="/src/pages/Home" element={<Home />} />
+                <Route path="/src/pages/About" element={<About />} />
+              </Routes>
+            </Router>
+            <Card>
+              <Button style={{ flexGrow: 1 }} onClick={signOut}>
+                Sign out
+              </Button>
+            </Card>
+          </Grid>
+        </ScrollView>
       )}
     </Authenticator>
   );
--- src/pages/TextToImage/TextToImagePage.tsx
+++ src/pages/TextToImage/TextToImagePage.tsx
@@ -1,13 +1,10 @@
-import { Card } from "@aws-amplify/ui-react";
 import TextToImage from "../../components/TextToImage/ImageFromTextGenerator";
 
 
 function TextToImagePage() {
   
     return (
-        <Card>
           <TextToImage />
-        </Card>
     );
   }
 
