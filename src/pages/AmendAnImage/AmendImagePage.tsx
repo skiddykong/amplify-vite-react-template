8307dbfb4440dc6caf88f5b6706c809e974429e1
Justin Cooke <justin.cooke@adrennial.com>
2024-07-08 15:26:53 +0100

    Add amend image

--- src/pages/AmendAnImage/AmendImagePage.tsx
+++ src/pages/AmendAnImage/AmendImagePage.tsx
@@ -1,11 +1,7 @@
-import { Card } from "@aws-amplify/ui-react";
 
 function AmendAnImage() {
     return (
-        <Card>
-            <h1>Amend an Image</h1>
-            <p>Amend an image generation is coming soon</p>
-        </Card>
+        <AmendAnImage />
     )
 }
 
