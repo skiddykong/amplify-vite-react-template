cfcfea77e401508df34bf5b77c0aa44aa10164e0
Justin Cooke <justin.cooke@adrennial.com>
2024-07-09 12:17:20 +0100

    Add amend image

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,10 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive'
-});
+    name: 'amplifyTeamDrive',
+    access: (allow) => ({
+      'unstaged/*': [
+        allow.authenticated.to(['read']),
+      ],
+    })
+  });
